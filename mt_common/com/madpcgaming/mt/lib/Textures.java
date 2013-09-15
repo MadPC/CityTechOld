8aba7709cc9d86ef6d2887b5b2e77909e8301e3a
Daniel Sapelli <daniel.sapelli@gmail.com>
2013-09-15 02:54:23 -0400

    Added the basic GUI stuff. Textures.java is for easy adding of the textures. ResourceLocationHelper is for textures

---
+++ mt_common/com/madpcgaming/mt/lib/Textures.java
@@ -0,0 +1,15 @@
+package com.madpcgaming.mt.lib;
+
+import net.minecraft.util.ResourceLocation;
+
+import com.madpcgaming.mt.helpers.ResourceLocationHelper;
+
+public class Textures
+{
+	//Base
+	public static final String GUI_SHEET_LOCATION = "textures/gui";
+	
+	//GUI
+	public static final ResourceLocation GUI_INDUSTRIAL_FURNACE = ResourceLocationHelper.getResourceLocation(GUI_SHEET_LOCATION + "industrialFurnace.png");
+	
+}
