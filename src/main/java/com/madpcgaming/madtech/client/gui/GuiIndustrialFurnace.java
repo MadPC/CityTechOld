ef76d1b09412682e0d420c2999143c90e7800fdf
Daniel Sapelli <daniel.sapelli@gmail.com>
2014-01-22 15:40:07 -0500

    Cleaned somethings up, fixed a naming.

--- src/main/java/com/madpcgaming/madtech/client/gui/GuiIndustrialFurnace.java
+++ src/main/java/com/madpcgaming/madtech/client/gui/GuiIndustrialFurnace.java
@@ -18,15 +18,14 @@ public class GuiIndustrialFurnace extends GuiContainer
 	public GuiIndustrialFurnace(InventoryPlayer playerInventory, TileEntityIndustrialFurnaceCore tileEntity)
 	{
 		super(new ContainerIndustrialFurnace(playerInventory, tileEntity));
-
 		this.tileEntity = tileEntity;
 	}
-
-	protected void drawGuiContainerForegroundLayer(int par1, int par2)
+	
+	@Override
+	protected void func_146979_b(int x, int y)
 	{
-		final String invTitle = Strings.GUI_INDUSTRIAL_FURNACE;
-		field_146289_q.drawString(invTitle, par1 / 2 - field_146289_q.getStringWidth(invTitle) / 2, 6, 4210752);
-		field_146289_q.drawString(StatCollector.translateToLocal("container.inventory"), 8,par2 - 96 + 2, 4210752);
+		field_146289_q.drawString(StatCollector.translateToLocal(Strings.GUI_INDUSTRIAL_FURNACE),28, 6, 4210752);
+		field_146289_q.drawString(StatCollector.translateToLocal("container.inventory"), 8, 176 - 96 + 2, 4210752);
 	}
 
 	@Override
@@ -34,7 +33,7 @@ public class GuiIndustrialFurnace extends GuiContainer
 	{
 		GL11.glColor4f(1f, 1f, 1f, 1f);
 
-		this.field_146297_k.renderEngine.bindTexture(Textures.GUI_INDUSTRIAL_FURNACE);
+		this.field_146297_k.getTextureManager().bindTexture(Textures.GUI_INDUSTRIAL_FURNACE);
 
 		int x = (field_146294_l - par1) / 2;
 		int y = (field_146295_m - par2) / 2;
@@ -44,11 +43,11 @@ public class GuiIndustrialFurnace extends GuiContainer
 		if(tileEntity.isBurning())
 		{
 			i1 = tileEntity.getBurnTimeRemainingScaled(12);
-			drawTexturedModalRect(x + 56, y + 36 + 12 - i1, 176, 12 - i1, 14, i1 + 2);
+			drawTexturedModalRect(x + 57, y + 36 + 23 - i1, 176, 12 - i1, 14, i1 + 2);
 		}
 		
 		i1 = tileEntity.getCookProgressScaled(24);
-		drawTexturedModalRect(x + 79, y + 34, 176, 14, i1 + 1, 16);
+		drawTexturedModalRect(x + 83, y + 34, 176, 14, i1 + 1, 16);
 	}
 
 }
