166d716cbec7bd54ec6cd40c2fec219b3df4216b
Daniel Sapelli <daniel.sapelli@gmail.com>
2014-01-31 16:11:49 -0500

    Reference Error. Fixes CommonProxy Crash

--- src/main/java/com/madpcgaming/citytech/lib/Reference.java
+++ src/main/java/com/madpcgaming/citytech/lib/Reference.java
@@ -3,12 +3,12 @@ package com.madpcgaming.citytech.lib;
 public class Reference
 {
 	
-	public static final String	MOD_ID				= "madtech";
-	public static final String	MOD_NAME			= "MadTech";
+	public static final String	MOD_ID				= "citytech";
+	public static final String	MOD_NAME			= "CityTech";
 	public static final String	VERSION				= "pre-release-0.0.1";
 	public static final String	CHANNEL_NAME		= MOD_ID;
-	public static final String	CLIENT_PROXY_CLASS	= "com.madpcgaming.madtech.core.proxy.ClientProxy";
-	public static final String	SERVER_PROXY_CLASS	= "com.madpcgaming.madtech.core.proxy.CommonProxy";
+	public static final String	CLIENT_PROXY_CLASS	= "com.madpcgaming.citytech.core.proxy.ClientProxy";
+	public static final String	SERVER_PROXY_CLASS	= "com.madpcgaming.citytech.core.proxy.CommonProxy";
 	
 	// different masks to be applied with bitwise-and
 	public static final byte	TOP					= 2;
